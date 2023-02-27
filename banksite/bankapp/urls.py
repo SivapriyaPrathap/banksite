84f815447af557c417952553e3d897aa96c9b831
macbook air <macbookair@MacBook-Air-van-macbook.local>
2023-02-27 10:35:27 +0100

    task

---
+++ banksite/bankapp/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+
+from bankapp import views
+
+app_name = 'bankapp'
+
+urlpatterns = [
+    path('', views.home, name="home"),
+    path('getdata', views.getdata, name="getdata"),
+    path('completed', views.completed, name="completed"),
+    path('register', views.register, name="register"),
+    path('login', views.login, name="login"),
+    path('logout', views.logout, name="logout"),
+
+]
---
+++ banksite/bankapp/views.py
@@ -0,0 +1,69 @@
+from django.contrib import messages, auth
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+
+from .models import District, branch
+
+
+# Create your views here.
+def getdata(request):
+    deptcontext = District.objects.all()
+    branchcontext = branch.objects.all()
+    if request.method == 'POST':
+
+        return redirect('/completed')
+
+    return render(request, "profile.html", {'District': deptcontext, 'branch': branchcontext})
+
+
+def home(request):
+    return render(request, "home.html")
+
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/getdata')
+        else:
+            messages.info(request, 'invalid login')
+            return redirect('/login')
+
+    return render(request, 'login.html')
+
+
+def register(request, ):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        confirm_password = request.POST['password1']
+        if password == confirm_password:
+            if User.objects.filter(username=username).exists():
+                messages.info(request, "username already exist")
+                return redirect('/register')
+
+            else:
+                user = User.objects.create_user(username=username,
+                                                password=password)
+                user.save
+                return redirect('/login')
+
+        else:
+            messages.info(request, 'password miss match')
+            return redirect('/register')
+        return redirect('/login')
+
+    return render(request, "register.html")
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
+
+
+def completed(request):
+    return render(request, "completed.html")
